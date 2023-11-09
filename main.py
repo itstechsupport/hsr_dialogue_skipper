9a281de8b420d81a5ccf6c36eb3f3852033da73d
itstechsupport <maciek.z.yt.pl@gmail.com>
2023-11-09 22:48:21 +0100

    ok should be fine now

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from win32api import GetSystemMetrics
 x = int(GetSystemMetrics(0)/2)
 y = int(GetSystemMetrics(1)*0.9375)
 
-print(getpixelcolor.pixel(x, y))
+print("MAKE SURE TO RUN ME AS ADMINISTRATOR :3")
+
 moveTo(x, y)
 while(True):
     px = getpixelcolor.pixel(x, y)
     if (px == (206, 190, 145)):
         moveTo(x, y)
         click()
+        print("Dialogue skipped!")

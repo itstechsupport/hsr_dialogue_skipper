4697ad01d5274576ce006996b566350c22bd9050
itstechsupport <maciek.z.yt.pl@gmail.com>
2023-11-09 23:16:08 +0100

    im so dumb

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from pyautogui import moveTo, click
 from win32api import GetSystemMetrics
 
 x = int(GetSystemMetrics(0)/2)
-y = int(GetSystemMetrics(1)*0.9375)
+y = int(GetSystemMetrics(1)*0.936)
 
 print("MAKE SURE TO RUN ME AS ADMINISTRATOR :3")
 
 while(True):
-    px = getpixelcolor.pixel(x, y)
-    if (px == (206, 190, 145)):
+    px = getpixelcolor.average(x-1, y, x+1, y+2)
+    print(px)
+    if (px == (3, 3, 3)):
         moveTo(x, y)
         click()
         print("Dialogue skipped!")
